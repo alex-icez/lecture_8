0606257018db6623f0d468c1aa9892ac91be270b
alex-icez <alex-icex@mail.ru>
2015-11-12 18:26:22 +0300

    mess

--- src/lecture_8/HomeWork.java
+++ src/lecture_8/HomeWork.java
@@ -1,9 +1,14 @@
 package lecture_8;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class HomeWork {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedImage in = ImageIO.read(new File("/home/sasha/3244317.jpg"));
 
 	}
 
