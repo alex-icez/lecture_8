03c039b1bc2cff554947dc6e7769d111106f3c16
alex-icez <alex-icex@mail.ru>
2015-11-12 20:51:21 +0300

    mess

--- src/lecture_8/Sample3.java
+++ src/lecture_8/Sample3.java
@@ -34,7 +34,7 @@ public class Sample3 {
 		
 		String s = "  Алгоритм Ахо — Аи Корасик — алгоритм  23 поиска   подстроки  ";
 		System.out.println(s.replaceAll("—", "#"));
-		System.out.println(s.replaceAll("[Аи]", "#"));
+		System.out.println(s.replaceAll("[А-Я]", "#"));
 		
 		
 		
