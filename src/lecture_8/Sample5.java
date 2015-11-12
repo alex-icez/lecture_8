d044df87a861f1dbde73e1f02ebe987d140b3843
alex-icez <alex-icex@mail.ru>
2015-11-12 21:07:11 +0300

    mess

--- src/lecture_8/Sample5.java
+++ src/lecture_8/Sample5.java
@@ -2,13 +2,17 @@ package lecture_8;
 
 public class Sample5 {
 
+	public static void fun(A a) {
+		
+	}
+	
 	public static void main(String[] args) {
 		class A {
 			
 			
 		};
 		
-		A a;
+		A a = new A();
 
 	}
 
