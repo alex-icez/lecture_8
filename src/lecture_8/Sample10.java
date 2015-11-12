40222749daef1c5c14cbe3ecbb1c755144e095f5
alex-icez <alex-icex@mail.ru>
2015-11-12 21:20:45 +0300

    mess

--- src/lecture_8/Sample10.java
+++ src/lecture_8/Sample10.java
@@ -6,8 +6,8 @@ public class Sample10 {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		String str = "авпвапва";
-		byte b[] = str.getBytes("cp1251");
-		System.out.println(new String(b, "cp1251"));
+		byte b[] = str.getBytes("utf-8");
+		System.out.println(new String(b, "utf-8"));
 		
 
 	}
