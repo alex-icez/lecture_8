a49f180aa15c938a66210cdf795b7831eff226e7
alex-icez <alex-icex@mail.ru>
2015-11-12 20:28:07 +0300

    mess

--- src/lecture_8/Sample3.java
+++ src/lecture_8/Sample3.java
@@ -4,7 +4,7 @@ public class Sample3 {
 	public static void main(String args[]) {
 		String s1 = "abc";
 		String s2 = "abc";
-		String s3 = s1.concat(s2);
+		String s3 = s1 + s2;
 		System.out.println(s3);
 	}
 }
