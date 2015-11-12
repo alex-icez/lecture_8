eb0d818387092856f30bf7261a3779c2a8ee2409
alex-icez <alex-icex@mail.ru>
2015-11-12 21:25:37 +0300

    mess

--- src/lecture_8/Sample10.java
+++ src/lecture_8/Sample10.java
@@ -2,18 +2,20 @@ package lecture_8;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Sample10 {
 
-	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		String str = "авпвапва";
 		byte b[] = str.getBytes("utf-8");
 		System.out.println(new String(b, "cp1251"));
 		
-		Socket s = new Socket("8.8.8.8");
+		Socket s = new Socket("8.8.8.8", 80);
 		InputStreamReader in = new InputStreamReader(new FileInputStream("gfd"), "utf8");
 
 		
