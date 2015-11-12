48e56c9c3222f1f55d16d97c5610a62dc91f4c67
alex-icez <alex-icex@mail.ru>
2015-11-12 21:19:08 +0300

    mess

--- src/lecture_8/Sample10.java
+++ src/lecture_8/Sample10.java
@@ -4,7 +4,7 @@ public class Sample10 {
 
 	public static void main(String[] args) {
 		String str = "авпвапва";
-		bytes b[] = str.getBytes("utf-8")
+		bytes b[] = str.getBytes("cp-1251");
 		
 		
 
