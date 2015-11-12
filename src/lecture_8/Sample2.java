85b570f680a251635e5f4660ec7445f194208876
alex-icez <alex-icex@mail.ru>
2015-11-12 19:55:30 +0300

    mess

--- src/lecture_8/Sample2.java
+++ src/lecture_8/Sample2.java
@@ -9,17 +9,18 @@ class MyException extends Exception {
 
 public class Sample2 {
 
-	static int read() {
+	static int read() throws MyException {
 		Scanner s = new Scanner(System.in);
 		int res = s.nextInt();
 		if (res == 10) {
 			MyException ex = new MyException();
 			throw ex;
 		}
+		return res;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		read();
 
 	}
 
