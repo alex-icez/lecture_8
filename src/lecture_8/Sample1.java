ec02252a8f07224f0c6e4f9535284c6663df9a6e
alex-icez <alex-icex@mail.ru>
2015-11-12 19:10:42 +0300

    mess

--- src/lecture_8/Sample1.java
+++ src/lecture_8/Sample1.java
@@ -16,11 +16,6 @@ public class Sample1 {
 	public static void main(String[] args) {
 		try {
 			f1();
-			class A { 
-				
-			}
-			
-			
 		} catch(NullPointerException e) {
 			e.printStackTrace();
 			System.out.println("Eeeee!!!");
