04a0b60d18a0eea3d2a5eb35111e764f01cf2b9c
alex-icez <alex-icex@mail.ru>
2015-11-12 20:30:16 +0300

    mess

--- src/lecture_8/Sample3.java
+++ src/lecture_8/Sample3.java
@@ -6,5 +6,7 @@ public class Sample3 {
 		String s2 = "abc";
 		String s3 = s1 + s2;
 		System.out.println(s3.charAt(2));
+		for(int i = 0; i < s3.length(); i++)
+			System.out.print(s3.charAt(i) + " ");
 	}
 }
