aa4340df663d201485f8efcba62f9d581fa44211
alex-icez <alex-icex@mail.ru>
2015-11-12 19:57:40 +0300

    mess

--- src/lecture_8/Sample2.java
+++ src/lecture_8/Sample2.java
@@ -3,9 +3,11 @@ package lecture_8;
 import java.util.Scanner;
 
 class MyException extends Exception {
+	int res;
+	public MyException(int res) {
+		this.res = res;
+	}
 }
-
-
 
 public class Sample2 {
 
@@ -13,15 +15,17 @@ public class Sample2 {
 		Scanner s = new Scanner(System.in);
 		int res = s.nextInt();
 		if (res == 10) 
-			throw new MyException();
+			throw new MyException(res);
 		return res;
 	}
 	
 	public static void main(String[] args) {
+		int r;
 		try {
 			read();
 		} catch (MyException e) {
 			e.printStackTrace();
+			r = e.res;
 		}
 	}
 
