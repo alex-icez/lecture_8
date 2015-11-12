1ec7f60e1865c58e91566c1f2b5c1197292af6b6
alex-icez <alex-icex@mail.ru>
2015-11-12 18:27:26 +0300

    mess

--- src/lecture_8/HomeWork.java
+++ src/lecture_8/HomeWork.java
@@ -2,13 +2,18 @@ package lecture_8;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class HomeWork {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedImage in = ImageIO.read(new File("/home/sasha/3244317.jpg"));
+		int h = in.getHeight();
+		int w = in.getWidth();
+		int r = 15;
+		
 
 	}
 
