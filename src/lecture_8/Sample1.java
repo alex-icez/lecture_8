b3f3026f4a5ff6d42bfce91e42fba60b19a06e8c
alex-icez <alex-icex@mail.ru>
2015-11-12 19:07:24 +0300

    mess

--- src/lecture_8/Sample1.java
+++ src/lecture_8/Sample1.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class Sample1 {
 
-	public static void main(String[] args) {
+	public static void f2() {
 		Scanner s = null;
+		int a = s.nextInt();
+	}
+	
+	public static void f1() {
+		f2();
+	}
+ 	
+	public static void main(String[] args) {
+		
 		try {
-			int a = s.nextInt();
+
 		} catch(NullPointerException e) {
 			System.out.println("Eeeee!!!");
 		}
