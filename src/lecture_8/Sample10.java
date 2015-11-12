9c42ad37a4d5cf1d0eb835ce37a703d6dc753290
alex-icez <alex-icex@mail.ru>
2015-11-12 21:23:59 +0300

    mess

--- src/lecture_8/Sample10.java
+++ src/lecture_8/Sample10.java
@@ -1,17 +1,19 @@
 package lecture_8;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
 public class Sample10 {
 
-	public static void main(String[] args) throws UnsupportedEncodingException {
+	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 		String str = "авпвапва";
 		byte b[] = str.getBytes("utf-8");
 		System.out.println(new String(b, "cp1251"));
 		
 		
-		InputStreamReader in = new InputStreamReader(new FileInputStream("gfd"));
+		InputStreamReader in = new InputStreamReader(new FileInputStream("gfd"), "utf8");
 
 	}
 
