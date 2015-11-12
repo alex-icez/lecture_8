e4317dce247e2a016b73f7ce266a08ab9d0809ca
alex-icez <alex-icex@mail.ru>
2015-11-12 21:10:56 +0300

    mess

--- src/lecture_8/Sample5.java
+++ src/lecture_8/Sample5.java
@@ -1,15 +1,13 @@
 package lecture_8;
 
-class Aa {
-	void print() {
-		System.out.println("Hello!!");
-	}
+interface Aa {
+	void print();
 }
 
 public class Sample5 {
 	public static void main(String[] args) {
 		Aa a = new Aa() {
-			void print() {
+			public void print() {
 				System.out.println("Dia!!");
 			}
 		};
