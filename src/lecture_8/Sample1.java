294f6bc3ced7912eb9775d4f1cca6413b23971a3
alex-icez <alex-icex@mail.ru>
2015-11-12 19:47:59 +0300

    mess

--- src/lecture_8/Sample1.java
+++ src/lecture_8/Sample1.java
@@ -17,13 +17,13 @@ public class Sample1 {
 			Scanner s = new Scanner(System.in);
 			String str = s.next();
 			int a = Integer.parseInt(str);
-		} catch(Exception e) {
-			
-		}	catch(NullPointerException e) {
+		} catch(NullPointerException e) {
 			e.printStackTrace();
 			System.out.println("Eeeee!!!");
 		} catch(NumberFormatException e) {
 			
+		} catch(Exception e) {
+			System.out.println("exc");
 		}
 		System.out.println("exit");
 	}
