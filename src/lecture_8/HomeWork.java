6c36cc9b376a423cbafe981eb55127832717124c
alex-icez <alex-icex@mail.ru>
2015-11-12 18:30:44 +0300

    mess

--- src/lecture_8/HomeWork.java
+++ src/lecture_8/HomeWork.java
@@ -12,9 +12,9 @@ public class HomeWork {
 		BufferedImage in = ImageIO.read(new File("/home/sasha/3244317.jpg"));
 		int w = in.getWidth();
 		int h = in.getHeight();
-		
 		int r = 15;
 		BufferedImage out = new BufferedImage(w, h, in.getType());
+		int rs, gs, bs;
 		for(int y = 0; y < h; y++) 
 			for(int x = 0; x < w; x++) {
 		
