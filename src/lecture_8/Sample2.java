f718198f42c93a4231a3b5b1e4b3079769954fc4
alex-icez <alex-icex@mail.ru>
2015-11-12 19:53:21 +0300

    mess

--- src/lecture_8/Sample2.java
+++ src/lecture_8/Sample2.java
@@ -1,14 +1,18 @@
 package lecture_8;
 
 class MyExeption extends Exception {
-	
-	
 }
 
 
 
 public class Sample2 {
 
+	static int read() {
+ 	
+	
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
